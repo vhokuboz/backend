053559c621d9c9f83b89cdda22eb2239374aa12d
Vitor Okubo <vitor.okubo@outlook.com>
2025-09-03 18:11:56 -0300

    Initial commit

---
+++ src/main/java/com/vitor/AgendaBankApplication.java
@@ -0,0 +1,11 @@
+package com.vitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaBankApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgendaBankApplication.class, args);
+    }
+}
